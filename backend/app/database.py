7f9d8d272d8666696babf7ac5180900f77718627
indynailuvar <indeeyynailuvar@gmail.com>
2026-05-30 17:35:17 +0700

    Add PostgreSQL database connection

---
+++ backend/app/database.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="restaurant",
+    user="postgres",
+    password="1234",
+    port=5434
+)
+
+cursor = conn.cursor()
+
+print("Connected to PostgreSQL!")
