6caeba2926c801f83cffa28edffd8d1bc1bc245f
Syavikarani Cahya Putri <vikaarnn09@gmail.com>
2026-06-02 00:08:39 +0700

    Include predict router in main application
    Added predict router to the FastAPI application.

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://restaurant-revenue-prediction-system-aon3n1b5g.vercel.app"
+        "https://restaurant-revenue-prediction-syste.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
