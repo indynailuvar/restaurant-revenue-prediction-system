87b286197649f2aa8d973fdf1f04ff1b3b429f3f
Syavikarani Cahya Putri <vikaarnn09@gmail.com>
2026-06-03 15:18:03 +0700

    Refactor CORS settings and include predict router

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,10 +16,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://restaurant-revenue-prediction-system-aon3n1b5g.vercel.app",
-        "https://restaurant-revenue-prediction-system-cu2qjqxyg.vercel.app",
+        "https://restaurant-revenue-prediction-dicto.vercel.app"
     ],
-    allow_origin_regex=r"https://restaurant-revenue-prediction-system-.*\.vercel\.app",
+    allow_origin_regex=r""https://restaurant-revenue-prediction-dicto.vercel.app",",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
